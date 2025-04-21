a4f97b8a91c832b43e9641665af9a0bfd9fe7fbe
ALAIN-NG <alain.ng.tech@gmail.com>
2025-04-21 16:13:57 +0100

    Deployement

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Middleware pour servir les fichiers statiques
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// Gestion des routes pour React Router
+app.use((req, res, next) => {
+  if (req.accepts('html')) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  } else {
+    next();
+  }
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log(`Environment: ${process.env.NODE_ENV}`);
+});
